447cafdc2a1ec69ddea2ecdd779345a6eb14be48
xaofao <ceemran.4@gmail.com>
2025-08-30 12:46:12 +0545

    updated

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    
+    <form action="database.php" method="POST">
+        <h2> Login Form</h2>
+        <label for=""> Username:</label>
+        <input type="text" name="username" >
+         <label for=""> Password:</label>
+        <input type="password" name="password" >
+        
+       <button type="submit" name="login"> Login</button>
+    </form>
+</body>
+</html>
